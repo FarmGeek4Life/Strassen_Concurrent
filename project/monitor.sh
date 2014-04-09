ff3aba7a34d5ce85eea8a4cdda8286779045d925
FarmGeek4Life <brysoncg@gmail.com>
2014-04-09 03:43:59 -0600

    Script to assist in tracking memory usage

--- project/monitor.sh
+++ project/monitor.sh
@@ -9,7 +9,7 @@ while [ $COUNT -le 20 ]
 do
    if [ $(ps aux | grep -E -v -e "grep|bash" | grep -E -c -e "strassen|client_manager|server_leaf") -gt 0 ]; then
       COUNT=0
-      echo "$(ps aux | grep -E -v -e "grep|bash" | grep -E -e "strassen|client_manager|server_leaf")" >> monitor.txt
+      echo "$(ps aux | grep -E -v -e "grep|bash" | grep -E -e "strassen|client_manager|server_slave")" >> monitor.txt
    else
       let COUNT+=1
    fi
